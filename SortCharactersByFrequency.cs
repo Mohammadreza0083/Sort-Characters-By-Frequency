926ef1532788edcc8de0a0567d2b9b44cb92bd2d
Mohammadreza Bonyadi <118369462+Mohammadreza0083@users.noreply.github.com>
2024-02-07 23:44:46 +0330

    Implemented FrequencySort method : in SortCharactersByFrequency class

--- SortCharactersByFrequency.cs
+++ SortCharactersByFrequency.cs
@@ -1,7 +1,26 @@
-﻿namespace Sort_Characters_By_Frequency
+﻿using System.Text;
+
+namespace Sort_Characters_By_Frequency
 {
     public static class SortCharactersByFrequency
     {
-
+        public static string FrequencySort(string input)
+        {
+            if (string.IsNullOrEmpty(input))
+            {
+                return "";
+            }
+            Stack<char> InputChars = new();
+            foreach (char c in input) 
+            { 
+                InputChars.Push(c);
+            }
+            StringBuilder OutPut = new StringBuilder();
+            foreach (char item in InputChars) 
+            {
+                OutPut.Append(item);
+            }
+            return OutPut.ToString();
+        }
     }
 }
